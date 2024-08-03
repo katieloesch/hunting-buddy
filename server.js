5b48b12ecc1241f40da9712c5b9aa2bb0acd91ed
Katie Loesch <catherine.loesch@protonmail.com>
2024-08-03 21:56:54 +0100

    set up GET route to get all jobs

--- server.js
+++ server.js
@@ -1,18 +1,23 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
-
 import express from 'express';
 const app = express();
-
 import morgan from 'morgan';
+import { nanoid } from 'nanoid';
 
+let jobs = [
+  { id: nanoid(), company: 'netflix', position: 'front-end' },
+  { id: nanoid(), company: 'wb', position: 'back-end' },
+];
+
+//add middleware to all routes with app.use()
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-//add middleware to all routes with app.use()
 app.use(express.json());
 
+//routes
 app.get('/', (req, res) => {
   res.send('hello world');
 });
@@ -20,6 +25,10 @@ app.get('/', (req, res) => {
 app.post('/', (req, res) => {
   console.log(req);
   res.json({ message: 'data received', data: req.body });
+});
+
+app.get('/api/v1/jobs', (req, res) => {
+  res.status(200).json({ jobs });
 });
 
 const port = process.env.PORT || 5100;
