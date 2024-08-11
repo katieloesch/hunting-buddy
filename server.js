44933d9049c5d666adc32644ecaa5c0eeb2310f1
Katie Loesch <catherine.loesch@protonmail.com>
2024-08-11 06:10:01 +0100

    add express-validator package to create custom validation middleware

--- server.js
+++ server.js
@@ -7,11 +7,12 @@ const app = express();
 
 import morgan from 'morgan';
 import mongoose from 'mongoose';
+import { validateTest } from './middleware/validationMiddleware.js';
 
 // routers
 import jobRouter from './routes/jobRouter.js';
 
-//middleware
+// middleware
 import errorHandlerMiddleware from './middleware/errorHandlerMiddleware.js';
 
 if (process.env.NODE_ENV === 'development') {
@@ -20,15 +21,22 @@ if (process.env.NODE_ENV === 'development') {
 
 app.use(express.json());
 
+// ---------------------------------------------------------------
+
 //routes
 app.get('/', (req, res) => {
   res.send('hello world');
 });
 
-app.post('/', (req, res) => {
-  console.log(req);
-  res.json({ message: 'data received', data: req.body });
-});
+app.post(
+  '/api/v1/test',
+  validateTest,
+
+  (req, res) => {
+    const { name } = req.body;
+    res.json({ message: `hello ${name}` });
+  }
+);
 
 app.use('/api/v1/jobs', jobRouter);
 
