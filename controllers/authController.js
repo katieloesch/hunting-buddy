d160fb5ba2ddff37712ea4baec847b7e9fa97300
Katie Loesch <catherine.loesch@protonmail.com>
2024-08-13 21:10:25 +0100

    update register function in authController -> first user registered should have 'admin' role, any other user has 'user' role

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,6 +2,9 @@ import { StatusCodes } from 'http-status-codes';
 import User from '../models/UserModel.js';
 
 export const register = async (req, res) => {
+  // if user is the first one created in collection, role should be admin
+  const isFirstAccount = (await User.countDocuments()) === 0;
+  req.body.role = isFirstAccount ? 'admin' : 'user';
   const user = await User.create(req.body);
   res.status(StatusCodes.CREATED).json({ user });
 };
