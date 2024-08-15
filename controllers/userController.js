3335f7221d9200cd805f21e620ee3bf93a47c266
Katie Loesch <catherine.loesch@protonmail.com>
2024-08-16 00:36:18 +0100

    add instance method to UserModel to omit password when sending userData to the frontend

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,7 +5,9 @@ import Job from '../models/JobModel.js';
 
 export const getCurrentUser = async (req, res) => {
   const user = await User.findOne({ _id: req.user.userId });
-  res.status(StatusCodes.OK).json({ user });
+  // user variable contains hashed pw, use instance method to make sure pw is removed and not sent to frontend
+  const userNoPw = user.toJSON();
+  res.status(StatusCodes.OK).json({ user: userNoPw });
 };
 
 export const getApplicationStats = async (req, res) => {
