01a7935620bf0dbfe377502f54f8a036ffd0f41d
Katie Loesch <catherine.loesch@protonmail.com>
2024-08-15 23:17:09 +0100

    add logout function to authController, add /logout route to authRouter, verify API endpoint

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
 import { StatusCodes } from 'http-status-codes';
+
 import User from '../models/UserModel.js';
 import { comparePasswords, hashPassword } from '../utils/passwordUtils.js';
 import { UnauthenticatedError } from '../errors/customErrors.js';
@@ -50,4 +51,12 @@ export const login = async (req, res) => {
   });
 
   res.status(StatusCodes.OK).json({ msg: 'user logged in' });
+};
+
+export const logout = (req, res) => {
+  res.cookie('token', 'logout', {
+    httpOnly: true,
+    expires: new Date(Date.now()),
+  });
+  res.status(StatusCodes.OK).json({ msg: 'user logged out' });
 };
