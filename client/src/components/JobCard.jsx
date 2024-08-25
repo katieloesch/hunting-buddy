32271286926a3833a5817a63c2f141e276a42313
Katie Loesch <catherine.loesch@protonmail.com>
2024-08-25 07:43:36 +0100

    create JobCard component and import it in JobContainer, render JobCard for each job retrieved from AllJobsContext

---
+++ client/src/components/JobCard.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const JobCard = () => {
+  return <h3>Job</h3>;
+};
+
+export default JobCard;
--- client/src/components/JobsContainer.jsx
+++ client/src/components/JobsContainer.jsx
@@ -1,7 +1,30 @@
 import React from 'react';
 
+import Wrapper from '../styledComponents/JobsContainer';
+import JobCard from './JobCard';
+import { useAllJobsContext } from '../pages/AllJobs';
+
 const JobsContainer = () => {
-  return <h2>JobsContainer</h2>;
+  const { data } = useAllJobsContext();
+  const { jobs } = data;
+
+  if (jobs.length === 0) {
+    return (
+      <Wrapper>
+        <h2>No jobs to display...</h2>
+      </Wrapper>
+    );
+  }
+
+  return (
+    <Wrapper>
+      <div className='jobs'>
+        {jobs.map((job) => (
+          <JobCard key={job._id} {...job} />
+        ))}
+      </div>
+    </Wrapper>
+  );
 };
 
 export default JobsContainer;
