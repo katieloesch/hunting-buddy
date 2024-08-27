5ff9fac1b93dd9e544094f52247917dff15d1974
Katie Loesch <catherine.loesch@protonmail.com>
2024-08-27 23:10:30 +0100

    make sure admin page is only accessible if the user role is admin

--- client/src/pages/Admin.jsx
+++ client/src/pages/Admin.jsx
@@ -1,7 +1,29 @@
 import React from 'react';
+import { useLoaderData, redirect } from 'react-router-dom';
+import { FaSuitcaseRolling, FaCalendarCheck } from 'react-icons/fa';
+import { toast } from 'react-toastify';
+
+import customFetch from '../utils/customFetch';
+import Wrapper from '../styledComponents/StatsContainer';
+
+export const loader = async () => {
+  try {
+    const response = await customFetch.get('/users/admin/app-stats');
+    return response.data;
+  } catch (error) {
+    toast.error('You are not authorised to view this page!');
+    return redirect('/dashboard');
+  }
+};
 
 const Admin = () => {
-  return <h2>Admin Page</h2>;
+  const { users, jobs } = useLoaderData();
+
+  return (
+    <Wrapper>
+      <h1>Admin</h1>
+    </Wrapper>
+  );
 };
 
 export default Admin;
