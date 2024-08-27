3a0f82e5ae2a520b909cb0ba1413048aded451e3
Katie Loesch <catherine.loesch@protonmail.com>
2024-08-27 23:28:28 +0100

    create StatsItem component, display current users and total jobs on admin page

--- client/src/pages/Admin.jsx
+++ client/src/pages/Admin.jsx
@@ -5,6 +5,7 @@ import { toast } from 'react-toastify';
 
 import customFetch from '../utils/customFetch';
 import Wrapper from '../styledComponents/StatsContainer';
+import { StatsItem } from '../components';
 
 export const loader = async () => {
   try {
@@ -21,7 +22,20 @@ const Admin = () => {
 
   return (
     <Wrapper>
-      <h1>Admin</h1>
+      <StatsItem
+        title='current users'
+        count={users}
+        color='#e99e49'
+        bg='#fcefc7'
+        icon={<FaSuitcaseRolling />}
+      />
+      <StatsItem
+        title='total jobs'
+        count={jobs}
+        color='#8b64cb'
+        bg='#e0e8f9'
+        icon={<FaCalendarCheck />}
+      />
     </Wrapper>
   );
 };
