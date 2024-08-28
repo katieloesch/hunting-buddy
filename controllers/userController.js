35c143b3a6a808c09283845385b5b9e77d5d466f
Katie Loesch <catherine.loesch@protonmail.com>
2024-08-28 01:39:57 +0100

    set up multer middleware and import it in userRouter (patch route)

--- controllers/userController.js
+++ controllers/userController.js
@@ -18,6 +18,7 @@ export const getApplicationStats = async (req, res) => {
 };
 
 export const updateUser = async (req, res) => {
+  console.log(req.file);
   const obj = { ...req.body };
   const updatedUser = await User.findByIdAndUpdate(req.user.userId, obj);
   res.status(StatusCodes.OK).json({ msg: 'update user...' });
