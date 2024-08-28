45658c67a3eea71a06d805c2af5bd2d424b51550
Katie Loesch <catherine.loesch@protonmail.com>
2024-08-28 02:22:17 +0100

    update LogoutContainer component to render uploaded img on frontend

--- client/src/components/LogoutContainer.jsx
+++ client/src/components/LogoutContainer.jsx
@@ -17,7 +17,12 @@ const LogoutContainer = () => {
           setShowLogout(!showLogout);
         }}
       >
-        <FaUserCircle />
+        {user.avatar ? (
+          <img src={user.avatar} alt='avatar' className='img' />
+        ) : (
+          <FaUserCircle />
+        )}
+
         {user && user.name}
         <FaCaretDown />
       </button>
