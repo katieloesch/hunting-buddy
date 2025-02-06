4c73dbb2902e953eca0f03f0491f33aa41a56943
Katie Loesch <catherine.loesch@protonmail.com>
2025-02-06 06:38:48 +0000

    set up StatsContainer component

--- client/src/components/StatsContainer.jsx
+++ client/src/components/StatsContainer.jsx
@@ -1,7 +1,47 @@
 import React from 'react';
+import {
+  FaSuitcaseRolling,
+  FaClock,
+  FaCalendarCheck,
+  FaBug,
+} from 'react-icons/fa';
+import { GiSandsOfTime } from 'react-icons/gi';
+import Wrapper from '../styledComponents/StatsContainer';
+import StatsItem from './StatsItem';
 
-const StatsContainer = () => {
-  return <h1>StatsContainer</h1>;
+const StatsContainer = (defaultStats) => {
+  const stats = [
+    {
+      title: 'pending applications',
+      count: defaultStats?.pending || 0,
+      icon: <FaClock />,
+      color: '#647acb',
+      bg: '#e0e8f9',
+    },
+    {
+      title: 'interviews scheduled',
+      count: defaultStats?.interview || 0,
+      icon: <FaCalendarCheck />,
+
+      color: '#eeae3e',
+      bg: '#fef3c7',
+    },
+    {
+      title: 'jobs declined',
+      count: defaultStats?.declined || 0,
+      icon: <FaBug />,
+      color: '#d66a6a',
+      bg: '#ffeeee',
+    },
+  ];
+
+  return (
+    <Wrapper>
+      {stats.map((stat) => (
+        <StatsItem key={stat.title} {...stat} />
+      ))}
+    </Wrapper>
+  );
 };
 
 export default StatsContainer;
