9f45ae2dab64b2d7f4bacfdf359fe15dad188c70
Katie Loesch <catherine.loesch@protonmail.com>
2025-02-08 16:46:00 +0000

    set up search form in SearchContainer component on frontend

--- client/src/components/SearchContainer.jsx
+++ client/src/components/SearchContainer.jsx
@@ -1,7 +1,48 @@
 import React from 'react';
+import { FormBtnSubmit, FormInput, FormSelect } from '.';
+import Wrapper from '../styledComponents/DashboardFormPage';
+import { Form, useSubmit, Link } from 'react-router-dom';
+import { JOB_TYPE, JOB_STATUS, JOB_SORT_BY } from '../../../utils/constants';
+import { useAllJobsContext } from '../pages/AllJobs';
 
 const SearchContainer = () => {
-  return <h2>SearchContainer</h2>;
+  return (
+    <Wrapper>
+      <Form className='form'>
+        <h5 className='form-title'>search form</h5>
+        <div className='form-center'>
+          <FormInput type='search' name='search' defaultValue='abc' />
+
+          <FormSelect
+            labelText='job status'
+            name='jobStatus'
+            options={['all', ...Object.values(JOB_STATUS)]}
+            defaultValue='all'
+          />
+
+          <FormSelect
+            labelText='job type'
+            name='jobType'
+            options={['all', ...Object.values(JOB_TYPE)]}
+            defaultValue='all'
+          />
+
+          <FormSelect
+            name='sort'
+            defaultValue='newest'
+            options={[...Object.values(JOB_SORT_BY)]}
+          />
+
+          <Link to='/dashboard/all-jobs' className='btn form-btn delete-btn'>
+            Reset Search Values
+          </Link>
+
+          {/* TEMP */}
+          <FormBtnSubmit formBtn />
+        </div>
+      </Form>
+    </Wrapper>
+  );
 };
 
 export default SearchContainer;
--- client/src/pages/AllJobs.jsx
+++ client/src/pages/AllJobs.jsx
@@ -6,6 +6,7 @@ import { JobsContainer, SearchContainer } from '../components';
 import customFetch from '../utils/customFetch';
 
 export const loader = async () => {
+  console.log('booyah!');
   try {
     const { data } = await customFetch.get('/jobs');
     return { data };
