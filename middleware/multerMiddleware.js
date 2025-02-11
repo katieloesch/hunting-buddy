f3afaac8f5186f60a943b69124a677522e4f4292
Katie Loesch <catherine.loesch@protonmail.com>
2025-02-11 15:14:40 +0000

    change multer storage from diskStorage to memoryStorage (multerMiddleware.js)

--- middleware/multerMiddleware.js
+++ middleware/multerMiddleware.js
@@ -1,14 +1,20 @@
 import multer from 'multer';
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, 'public/uploads');
-  },
-  filename: (req, file, cb) => {
-    const fileName = file.originalname;
-    cb(null, fileName);
-  },
-});
+// const storage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, 'public/uploads');
+//   },
+//   filename: (req, file, cb) => {
+//     const fileName = file.originalname;
+//     cb(null, fileName);
+//   },
+// });
+
+//
+
+// will upload image as buffer using datauri as disk space on render not available with the free version
+const storage = multer.memoryStorage();
 
 const upload = multer({ storage });
+
 export default upload;
