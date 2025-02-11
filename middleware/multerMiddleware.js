8ad515b1c6bb20f856020f0fa25f278582194094
Katie Loesch <catherine.loesch@protonmail.com>
2025-02-11 15:35:21 +0000

    update userController to fix image upload issue

--- middleware/multerMiddleware.js
+++ middleware/multerMiddleware.js
@@ -1,4 +1,6 @@
 import multer from 'multer';
+import DataParser from 'datauri/parser.js';
+import path from 'path';
 
 // const storage = multer.diskStorage({
 //   destination: (req, file, cb) => {
@@ -10,11 +12,16 @@ import multer from 'multer';
 //   },
 // });
 
-//
-
 // will upload image as buffer using datauri as disk space on render not available with the free version
 const storage = multer.memoryStorage();
 
 const upload = multer({ storage });
+
+const parser = new DataParser();
+
+export const formatImage = (file) => {
+  const fileExtension = path.extname(file.originalname).toString();
+  return parser.format(fileExtension, file.buffer).content;
+};
 
 export default upload;
